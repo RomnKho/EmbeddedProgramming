0075979286020cb704c3732dd7e8d10d5a4639a5
RomnKho <rkhokho@upv.edu.es>
2026-05-01 17:18:48 +0200

    Succesfully read potentiometer

--- ADC_test/main/main.c
+++ ADC_test/main/main.c
@@ -14,6 +14,18 @@
 #include "driver/adc.h"         // Implementes the adc channels
 #include "esp_adc_cal.h"        // Implements the calibration of the adc
 
+#define DEFAULT_VREF    1100
+#define NO_SAMPLES      64
+
+static const adc_bits_width_t   bit_width   = ADC_WIDTH_BIT_12; // if adc2, the bid width has to be specified 
+                                                                // each time adc2_get_raw() si called
+static const adc1_channel_t      channel    = ADC_CHANNEL_0;    // adc1_channel_0 => gpio 36
+static const adc_atten_t        attenuator  = ADC_ATTEN_DB_11;  // For potentiometers => atten. 150mV - 2450 mV
+static const adc_unit_t         unit        = ADC_UNIT_1;
+
+
+static esp_adc_cal_characteristics_t *adc_chars;                // Calibration characteristics
+
 void check_efuse(void)
 {
     if(esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK)
@@ -35,7 +47,50 @@ void check_efuse(void)
     }
 }
 
+void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if(val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+    {
+        printf("Characterized using Two Point Calibration\n");
+    }
+    else if(val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
+    {
+        printf("Characterized using Vref Calibration\n");
+    }
+    else
+    {
+        printf("Characterized using Default Vref Calibration\n");
+    }
+}
+
 void app_main(void)
 {
     check_efuse();
+
+    // Configure the ADC
+    adc1_config_width(bit_width);
+    adc1_config_channel_atten(channel, attenuator);
+
+    // Characterize the ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, attenuator, bit_width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    for(;;)
+    {
+        uint32_t adc_reading = 0;
+        
+        // Multisampling
+        // One read = 20us, 64 reads = 1.28 ms
+        for(int i = 0; i < NO_SAMPLES; i++)
+        {
+            adc_reading += adc1_get_raw(channel);
+        }
+
+        adc_reading /= NO_SAMPLES;
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %lu | Voltage = %lu mV\n", adc_reading, voltage);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
